06a25a1cb6c8d30b3aaca6a265c03597fdbefeae
Janengh <vsemnazlo.kd@gmail.com>
2019-03-27 22:46:52 +0200

    fixed test

--- test_programm.asm
+++ test_programm.asm
@@ -2,34 +2,31 @@ DATA SEGMENT
 	VB		DB	111111b
 	VD		DD	0d34dah
 	VW		DW	5d
-	Stringd	DB	"DoroshKaruna"
+	Strin	DB	"DoroshKaruna"
 	stri	DB 	"A"
 DATA ENDS
-	
 CODE SEGMENT
 	mov bl, 11000b
+	cmp bl, VB
+	jge label2
 EXMPL MACRO 	
-	and VB, bl		;direct adressing
+	and VB, bl		
 	or	VB, 110	
 ENDM  
 EXMPL
 	mov ax, 0d5h
-	dec word ptr GS:[si+1]			;basic adressing
+	dec word ptr GS:[si+1]			
 	add word ptr VD, ax			
 EXMPL2 MACRO NUM
 	inc NUM
 ENDM	
-EXMPL2 6
-	mov si, offset String
-	mov di, offset stri
-	
+EXMPL2 ax
 	mov ax, 0
 	mov cx, 8
-@label2:
-	inc ax
-	cmp VW, ax
+label2:
+	inc al
+	cmp ax, VW
 	movsw
-	jge @label2
-
+	jge label2
 CODE ENDS
 END
