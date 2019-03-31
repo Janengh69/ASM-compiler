6386d50831a25655b33e1245dcf1350854152bbf
Janengh69 <vsemnazlo.kd@gmail.com>
2019-03-31 19:15:32 +0300

    macro added

--- LexicalAnalyser.py
+++ LexicalAnalyser.py
@@ -1,10 +1,5 @@
 import re
-
-SYMBOLS = [ '[', ']', '.', ',', '_', '@','+', '-', ':']
-COMANDS = [ "MOV", "ADD", "DEC", "CMP", "AND", "OR", "INC", "JGE", "MOVSW"]
-DIRECTIVE = ['END', "DATA", "SEGMENT", "MACRO", "ENDM", "DB", "DW", "DD", "CODE", "ENDS", "WORD", "PTR", "OFFSET"]
-REGISTER16 = [ 'AX', 'BX', 'CX', 'DX']
-REGISTER8 = ['AH', 'BH', 'CH', 'DH', 'AL', 'BL', 'CL', 'DL']
+import Common as Com
 
 def error( msg):
     print ('Lexer error: ', msg)
@@ -12,9 +7,9 @@ def error( msg):
 def AsmFileToList(filename):
     file = open(filename, "r")
     programm = list()
-    flag = False
     for line in file:
-        match = ''.join(re.findall("[a-zA-Z0-9:;+\" ' ',. '\t' @ \[ \]]", line))
+        match = ''.join(re.findall("[a-zA-Z0-9:;+\" ' ',. '\t' \[ \]]", line))
+        # replacing all punktuals to find them easy later 
         if match.find(',') != -1:
             match = match.replace(match[match.find(',') ], ' ,')
         if match.find(':') != -1:
@@ -30,80 +25,246 @@ def AsmFileToList(filename):
         if match.find('\t') != -1:
             match = match.replace(match[match.find('\t') : match.find('\t')+1], ' ')
         programm.append(match.split(" "))
-    for word in programm:
+    for word in programm: # deleting extra empty lists
         for i in word:
             if i == '':
                 word.remove(i)
+    macro_search(programm) 
     return programm
 
-def check_is_comand(word):
-    for temp in COMANDS:
-        if word.upper() == temp:
-            return True
-    return False
+def macro_search(lst):
+    count = -1
+    macro_flag = False
+    for row in lst:
+        if any("MACRO" in s for s in row):# checing if there are any MACRO in list
+            Com.macro_buf.append([])
+            count +=1
+            macro_flag = True              # there is macro 
+            Com.macro_format.append(row[0])
+            if len(row) > 2:                    # in case MACRO has parametrs
+                Com.macro_param.append(row[2])
+            #Com.macro_buf[count].append(row)
+            continue
+        if macro_flag:      #recording all the MACRO in macro_buf list
+            if any("ENDM" in s for s in row):
+                macro_flag = False 
+            Com.macro_buf[count].append(row)  # end of macro 
+   # print(Com.macro_buf)
+   # print(Com.macro_format)
+   # print(Com.macro_param)
+        
+def check_is_mnem(word):
+    for temp in Com.MNEM:
+        for temp in Com.MNEM:
+            if word.upper() == temp:
+                return True
+        return False
 
 def check_is_symbol(word):
-    for temp in SYMBOLS:
-        if word == temp:
-            return True
-    return False
+    for temp in Com.SYMBOLS:
+        for temp in Com.SYMBOLS:
+            if word == temp:
+                return True
+        return False
 
 def check_is_directive(word):
-    for temp in DIRECTIVE:
-        if word.upper() == temp:
-            return True
-    return False
+    for temp in Com.DIRECTIVE:
+        for temp in Com.DIRECTIVE:
+            if word.upper() == temp:
+                return True
+        return False
 
 def check_is_register8(word):
-    for temp in REGISTER8:
-        if word.upper() == temp:
-            return True
-    return False
+    for temp in Com.REGISTER8:
+        for temp in Com.REGISTER8:
+            if word.upper() == temp:
+                return True
+        return False
 
 def check_is_register16(word):
-    for temp in REGISTER16:
-        if word.upper() == temp:
-            return True
-    return False
+    for temp in Com.REGISTER16:
+        for temp in Com.REGISTER16:
+            if word.upper() == temp:
+                return True
+        return False
+def check_is_macro(word):
+    for temp in Com.MACRO:
+        for temp in Com.MACRO:
+            if word.upper() == temp:
+
+                return True
+        return False
+def check_is_segment_id(word):
+    for temp in Com.ID_SEGMENT:
+        for temp in Com.ID_SEGMENT:
+            if word.upper() == temp:
+                return True
+        return False
+def check_is_segment(word):
+    for temp in Com.SEGMENT:
+        for temp in Com.SEGMENT:
+            if word.upper() == temp:
+                return True
+        return False
 
 def output(list1):
-    for word in list1:
-        print(word)
+    for row in list1:
+        print()
+        for word in row:
+            print("|  ", end = '')
+            for i in range(len(word)):
+                print(word[i], " ",end = '')
+            print(" |", end = '')
+        print()
 
 def list_to_table(lst):
-    result = list()
-    row = dict()
+    result = list() 
+    pos = 0
+    user_macro = False
     flag = False
+    param_flag = True
     for word in lst:
-        for i in word: 
-            row = dict()
-            if check_is_comand(i):
-                row = [i.upper(), "COMAND", len(i)]
+        if word:
+            result.append([])
+        else: 
+            continue
+        for i in range(len(word)):      #describing every word (lexical analysis)
+            row = list()
+            if check_is_mnem(word[i]):
+                row = [word[i].upper(), "MNEM", len(word[i])]
                 flag = True
-            elif check_is_directive(i):
-                row = [ i.upper(), "DIRECTIVE", len(i)]
+                param_flag = True
+            elif check_is_macro(word[i]):
+                row = [ word[i].upper(), "MACRO", len(word[i])]
                 flag = True
-            elif check_is_symbol(i):
-                row = [ i, "SYMBOL", len(i)]
+            elif check_is_segment_id(word[i]):
+                row = [ word[i].upper(), "ID_SEGMENT", len(word[i])]
                 flag = True
-            elif check_is_register8(i):
-                row = [ i.upper(), "REGISTER8", len(i)]
+            elif check_is_directive(word[i]):
+                row = [ word[i].upper(), "DIRECTIVE", len(word[i])]
                 flag = True
-            elif check_is_register16(i):
-                row = [ i.upper(), "REGISTER16", len(i)]
+            elif check_is_segment(word[i]):
+                row = [ word[i].upper(), "SEGMENT", len(word[i])]
                 flag = True
-            elif i == '':
+            elif check_is_symbol(word[i]):
+                row = [ word[i], "SYMBOL", len(word[i])]
+                flag = True
+            elif check_is_register8(word[i]):
+                row = [ word[i].upper(), "REGISTER8", len(word[i])]
+                flag = True
+            elif check_is_register16(word[i]):
+                row = [ word[i].upper(), "REGISTER16", len(word[i])]
+                flag = True
+            elif word[i] == '':
                 continue
-            elif i == ''.join(re.findall(r'0+[0-9a-fA-F]+[hH]', i)) or i == ''.join(re.findall(r'[01]+[bB]', i)) or i == ''.join(re.findall(r'[0-9]+[dD]?', i)):
-                row = [ i, "NUMBER", len(i)]
-            elif i == ''.join(re.findall(r'[A-Z|a-z?.\d+]{1,6}', i)) and not flag:
-                    row = [ i.upper(), "USER", len(i)]
-            elif i == ''.join(re.findall(r'[/"\w/"]+', i)):
-                row = [ i, "TEXTCONST", len(i)] 
-            elif i == ''.join(re.findall(r'[@][a-z]+[0-9]+', i)) and not flag:
-                row = [ i, "MITKA", len(i)]
-            elif(not row): continue
-            result.append(row)
+            elif word[i] == ''.join(re.findall(r'0+[0-9a-fA-F]+[hH]', word[i])) or word[i] == ''.join(re.findall(r'[01]+[bB]', word[i])) or word[i] == ''.join(re.findall(r'[0-9]+[dD]?', word[i])):
+                row = [ word[i], "NUMBER", len(word[i])]
+                flag = True
+            elif word[i] == ''.join(re.findall(r'[@][a-z]+[0-9]+', word[i])) and not flag:
+                row = [ word[i], "LABLE", len(word[i])]
+            elif word[i] == ''.join(re.findall(r'[A-Z|a-z?.\d+]', word[i])) and len(word[i]) <= 6 and not flag:
+                for k in range(len(Com.macro_format)):
+                    if Com.macro_format[k] == word[i]:
+                        if len(word) == 1 or (len(word) == 2 and word[1] != "MACRO"):   #in case we call macro in program 
+                            if len(word) == 2:
+                                param = word[1]      
+                            param_flag = False
+                            for x in Com.macro_buf[k]:
+                                    row = macro_to_lex(x)               #lexical analysis for macro 
+                                    if row != None:
+                                        for rw in row:                  #replacing formal parametr into actual one
+                                            if rw[1] == "USER_MACRO_PARAM":
+                                                rw[0] = param.upper()
+                                        result.append(row)
+                                        pos+=1
+                            break
+                        row = [word[i].upper(), "USER_MACRO", len(word[i])]
+                        user_marco = True
+                        break
+                    if not user_macro:
+                        for param in Com.macro_param:
+                            if word[i].upper() == param:
+                                row = [ word[i].upper(), "USER_MACRO_PARAM", len(word[i])]
+                                break
+                            else:
+                                row = [ word[i].upper(), "USER", len(word[i])]
+                                user_macro = False
+            elif word[i][0] == '"' and word[i][len(word[i])-1] == '"' and not flag: 
+                 word[i] = ''.join(re.findall(r'[\"A-Z|a-z|\d+\"]', word[i]))
+                 row = [ word[i], "TEXTCONST", len(word[i])]
+            elif word[i] == ''.join(re.findall(r'[A-Z|a-z|\d+]', word[i])) and not flag:
+                if len(word[i]) > 6:
+                    row = [ word[i].upper, "UNDERFINED", len(word[i])]
+            if param_flag and row != None:
+                result[pos].append(row)
             flag = False
-    #output(result)
+            user_macro = False
+        pos +=1
+    print(result)
+    return(result)
+
+def macro_to_lex(lst):
+    result = list() 
+    flag = False
+    pos = 0
+    user_macro = False
+    for word in lst:
+        if word:
+            row = list()
+            if check_is_mnem(word):
+                row = [word.upper(), "MNEM", len(word)]
+                flag = True
+            elif check_is_macro(word):
+                row = [ word.upper(), "MACRO", len(word)]
+                flag = True
+            elif check_is_segment_id(word):
+                row = [ word.upper(), "ID_SEGMENT", len(word)]
+                flag = True
+            elif check_is_directive(word):
+                row = [ word.upper(), "DIRECTIVE", len(word)]
+                flag = True
+            elif check_is_segment(word):
+                row = [ word.upper(), "SEGMENT", len(word)]
+                flag = True
+            elif check_is_symbol(word):
+                row = [ word, "SYMBOL", len(word)]
+                flag = True
+            elif check_is_register8(word):
+                row = [ word.upper(), "REGISTER8", len(word)]
+                flag = True
+            elif check_is_register16(word):
+                row = [ word.upper(), "REGISTER16", len(word)]
+               # flag = True
+            elif word == '':
+                continue
+            elif word == ''.join(re.findall(r'0+[0-9a-fA-F]+[hH]', word)) or word == ''.join(re.findall(r'[01]+[bB]', word)) or word == ''.join(re.findall(r'[0-9]+[dD]?', word)):
+                row = [ word, "NUMBER", len(word)]
+            elif word == ''.join(re.findall(r'[@][a-z]+[0-9]+', word)) and not flag:
+                row = [ word, "LABLE", len(word)]
+            elif word == ''.join(re.findall(r'[A-Z|a-z?.\d+]', word)) and len(word) <= 6 and not flag:
+                for k in range(len(Com.macro_format)):
+                    if Com.macro_format[k] == word:
+                        row = [word.upper(), "USER_MACRO", len(word)] 
+                        user_marco = True
+                        continue
+                    if not user_macro:
+                        for param in Com.macro_param:
+                            if word.upper() == param:
+                                row = [ word.upper(), "USER_MACRO_PARAM", len(word)]
+                                break
+                            else:
+                                row = [ word.upper(), "USER", len(word)]
+                                user_macro = False
+            elif word[0] == '"' and word[len(word)-1] == '"' and not flag: 
+                    word = ''.join(re.findall(r'[\"A-Z|a-z|\d+\"]', word))
+                    row = [ word, "TEXTCONST", len(word)]
+            elif word == ''.join(re.findall(r'[A-Z|a-z|\d+]', word)) and not flag:
+                if len(word) > 6:
+                    row = [ word.upper, "UNDERFINED", len(word)]
+            result.append(row)
+            pos+=1
+            flag = False
+        else: 
+            continue
+    print(result)
     return(result)
